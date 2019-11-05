375a16a41a64e2020a0b946f6d6f8433262a1976
scott <1@qq.com>
2019-11-05 10:51:20 +0800

    '完成历史开奖和玩法规则页面开发'

--- src/views/play-method-rule/index.tsx
+++ src/views/play-method-rule/index.tsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import PlayExplain from './PlayExplain'
+import LotteryExplain from './LotteryExplain'
+import './index.styl'
+
+interface State {
+  tabIndex: number;
+}
+
+const tabs = ['玩法说明', '彩种说明']
 
 @inject("store")
 @observer
 class PlayMethodRule extends Component<Props, {}> {
+  state: State
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      tabIndex: 0
+    }
+  }
+
+  getComp = () => {
+    return this.state.tabIndex === 0 ? <PlayExplain /> : <LotteryExplain />
+  }
+
   render() {
-    console.log(this.props.store.user.name);
     return (
       <article className="play-method-rule-view">
-        PlayMethodRule
+        {
+          tabs.map((tab, idx) => <div key={tab} className={`page-title ${ idx === this.state.tabIndex ? 'active' : '' }`} onClick={() => this.setState({tabIndex: idx})}>{ tab }</div>)
+        }
+        <div className="rule-container m-t-1">
+          {this.getComp()}
+        </div>
       </article>
     )
   }
